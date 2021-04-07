70090d51ac5f22cbe2984f67ac9d7d75d8f01ee4
SpiderBeaver <spdrbeaver@gmail.com>
2021-04-07 11:51:56 +0300

    Implement rooms creating and joining.

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,20 @@ io.on('connection', (socket) => {
   socket.on('message', (args) => {
     console.log(args);
   });
+
+  socket.on('CREATE_ROOM', () => {
+    const roomId = Math.random().toString(36).substr(2, 9);
+    socket.join(`gameroom:${roomId}`);
+    socket.emit('CREATED_ROOM', { roomId: roomId });
+  });
+
+  socket.on('JOIN_ROOM', ({ roomId }: { roomId: string }) => {
+    const roomIdFull = `gameroom:${roomId}`;
+    if (io.sockets.adapter.rooms.has(roomIdFull)) {
+      socket.join(roomIdFull);
+      socket.emit('JOINED_ROOM', { roomId: roomId });
+    }
+  });
 });
 
 const port = 3001;
