74f0c51237980cd1e135a647bbdb548f7559f07b
SpiderBeaver <spdrbeaver@gmail.com>
2021-04-07 11:02:36 +0300

    Install socket.io. Remove some unused stuff.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('hi');
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('New connection');
+
+  socket.on('message', (args) => {
+    console.log(args);
+  });
+});
+
+const port = 3001;
+httpServer.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
